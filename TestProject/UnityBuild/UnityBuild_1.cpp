4e9de43270f6c60c1cf531218c772a5ea581962e
melony <yeop0422@naver.com>
2018-12-26 11:40:09 +0900

    테스트 프로젝트 수정

--- TestProject/UnityBuild/UnityBuild_1.cpp
+++ TestProject/UnityBuild/UnityBuild_1.cpp
@@ -2,5 +2,3 @@
 
 #include "..\main.cpp"
 #include "..\TestClass1.cpp"
-#include "..\Filter1\TestClass2.cpp"
-#include "..\Filter1\TestFunc1.cpp"
---
+++ TestProject/UnityBuild/UnityBuild_2.cpp
@@ -0,0 +1,4 @@
+#include "StdAfx.h"
+
+#include "..\Filter1\TestClass2.cpp"
+#include "..\Filter1\TestFunc1.cpp"
